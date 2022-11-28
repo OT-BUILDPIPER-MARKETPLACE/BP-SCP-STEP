0e4b0ba05ed0c7b9dc7f5cf0759fc6e98f83b10c
Sandeep <sandeep@opstree.com>
2022-11-28 08:37:23 +0530

    Added code to copy file from local to remote server

--- build.sh
+++ build.sh
@@ -18,12 +18,30 @@ fi
 
 logInfoMessage "I'll perform [action: ${ACTION}]" 
 
-logInfoMessage "Received below arguments"
-logInfoMessage "Action to be performed: ${ACTION}"
+
 
 case ${ACTION} in
     LOCAL_TO_REMOTE)
         logInfoMessage "Have to do copy operation from local system to remote"
+        if [ `isStrNonEmpty $LOCAL_FILE_PATH` -ne 0 ]
+        then
+            TASK_STATUS=1
+            logErrorMessage "Local file path not provided please check"
+        elif [ `isStrNonEmpty $TARGET_SERVER_ALIAS` -ne 0 ]
+        then
+            TASK_STATUS=1
+            logErrorMessage "Target server not provided please check"
+        elif [ `isStrNonEmpty $TARGET_FILE_PATH` -ne 0 ]
+        then
+            TASK_STATUS=1
+            logErrorMessage "Target file path provided please check"
+        fi   
+        logInfoMessage "Received below arguments"
+        logInfoMessage "Action to be performed: ${ACTION}"
+        logInfoMessage "Local file path: ${LOCAL_FILE_PATH}"
+        logInfoMessage "Target server alias: ${TARGET_SERVER_ALIAS}"
+        logInfoMessage "Target file path: ${TARGET_FILE_PATH}"
+        logInfoMessage "Command to be executed: scp ${LOCAL_FILE_PATH} ${TARGET_SERVER_ALIAS}:${TARGET_FILE_PATH}"
     ;;
     REMOTE_TO_LOCAL)
         logInfoMessage "Have to do copy operation from remote system to local"
